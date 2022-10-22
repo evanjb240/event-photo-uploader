11b9b5b3de2bbeeb40a2dbf9a52c51d75546ecb4
evanjb240 <evanjb@att.net>
2022-10-22 17:07:36 -0500

    navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,24 +1,103 @@
+<script>
+import { MenuIcon } from '@zhuowenli/vue-feather-icons'
+
+export default {
+    data() {
+        return {
+            nav: "topnav"
+        }
+    },
+    mounted() {
+    },
+    components: {
+        MenuIcon
+    },
+    methods: {
+        navBar() {
+            if (this.nav === "topnav") {
+                this.nav += ' responsive';
+
+            } else {
+                this.nav = 'topnav';
+            }
+        }
+    }
+}
+</script>
+
 <template>
-    <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
-        <div class="container-fluid">
-            <a class="navbar-brand">Photo Uploader</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
-                <ul class="navbar-nav flex-grow-1">
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="/Uploader">Photo Upload</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-dark" href="/Privacy">Privacy</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <nav :class="nav">
+        <NuxtLink to="/" @click="nav = 'topnav'">Photo Uploader</NuxtLink>
+        <NuxtLink to="/" @click="nav = 'topnav'">Home</NuxtLink>
+        <NuxtLink to="/Uploader" @click="nav = 'topnav'">Photo Upload</NuxtLink>
+        <NuxtLink to="Privacy" @click="nav = 'topnav'">Privacy</NuxtLink>
+        <a href="javascript:void(0);" class="icon">
+            <menu-icon size="1.5x" @click="navBar"></menu-icon>
+        </a>
     </nav>
 </template>
+
+<style>
+body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+    overflow: hidden;
+    background-color: #333;
+}
+
+.topnav a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+.topnav a.active {
+    background-color: #04AA6D;
+    color: white;
+}
+
+.topnav .icon {
+    display: none;
+}
+
+@media screen and (max-width: 600px) {
+    .topnav a:not(:first-child) {
+        display: none;
+    }
+
+    .topnav a.icon {
+        float: right;
+        display: block;
+    }
+}
+
+@media screen and (max-width: 600px) {
+    .topnav.responsive {
+        position: relative;
+    }
+
+    .topnav.responsive .icon {
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .topnav.responsive a {
+        float: none;
+        display: block;
+        text-align: left;
+    }
+}
+</style>
