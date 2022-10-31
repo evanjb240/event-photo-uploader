bad29cfc2b39c438db6c3d3755619aee55ddc03b
evanjb240 <evanjb@att.net>
2022-10-31 11:17:25 -0500

    Blob Storage setup

--- api/Uploader.cs
+++ api/Uploader.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Events.Services;
 
 namespace Events.Uploader
 {
@@ -17,19 +18,32 @@ namespace Events.Uploader
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                var formData = await req.ReadFormAsync();
 
-            string name = req.Query["name"];
+                if (formData?.Files?.Count > 0)
+                {
+                    BlobStorage _blobStorage = new BlobStorage("profile-uploads", Environment.GetEnvironmentVariable("UploadStorage"));
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+                    var file = formData.Files[0];
+                    var filePath = Path.GetTempFileName();
+                    Guid fileNamePrefixGuid = Guid.NewGuid();
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                    using (var stream = new MemoryStream())
+                    {
+                        file.CopyToAsync(stream).ConfigureAwait(false);
+                        _blobStorage.UploadFile(fileNamePrefixGuid + file.FileName, stream);
+                    }
+                }
+                return new JsonResult(new { message = "Photo upload succeeded!" });
+            }
+            catch (Exception)
+            {
+                return new JsonResult(new { message = "Photo failed to upload." });
+            }
 
-            return new OkObjectResult(responseMessage);
+
         }
     }
 }
--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -1,16 +1,108 @@
-
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="drop-zone col-md-4 offset-md-4">
-                <span class="drop-zone__prompt">Click to take a picture!</span>
-                <input id="file-input" type="file" name="myFile" class="drop-zone__input" capture="environment">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-4"></div>
-            <button id="upload-button" class="upload-button primary-button col-md-4">Upload!</button>
-            <div class="col-md-4"></div>
-        </div>
-    </div>
+  <DropZone class="drop-zone" @files-dropped="addFiles" #default="{ dropZoneActive }">
+    <label class="drop-zone__prompt" for="file-input">
+      <span v-if="dropZoneActive">
+        <span>Drop Them Here </span>
+        <span class="smaller">to add them</span>
+      </span>
+      <span v-else>
+        <span>Drag Your Files Here</span>
+        <span class="smaller">
+          or <strong><em>click here</em></strong> to select files
+        </span>
+      </span>
+
+      <input class="drop-zone__input" type="file" id="file-input" @change="onInputChange" />
+    </label>
+    <ul class="image-list" v-show="files.length">
+      <FilePreview v-for="file of files" :key="file.id" :file="file" tag="li" @cancel="removeFile" />
+    </ul>
+  </DropZone>
+  <button class="upload-button" @click="upload">Upload</button>
+  <label v-if="uploadResult">{{uploadResultMessage}}</label>
 </template>
+
+<script setup>
+import useFileList from '../compositions/file-list.js'
+import DropZone from './DropZone.vue'
+import FilePreview from './FilePreview.vue'
+//import ref from 'vue';
+
+const { files, addFiles, removeFile } = useFileList()
+defineProps({
+  uploadResult:{
+    type:Boolean,
+    default:false
+  },
+  uploadResultMessage:{
+    type:String,
+    default:''
+  }
+})
+
+function onInputChange(e) {
+  addFiles(e.target.files)
+  e.target.value = null
+}
+
+function upload() {
+  var data = new FormData();
+
+  data.append('file', files.value[0].file);
+
+  fetch('/api/Uploader', {
+    method: 'POST',
+    body: data
+  }).then((response) => response.json())
+  .then((data) => {
+    uploadResult = true;
+    uploadResultMessage = data
+  })
+  .catch((error) => {
+    uploadResult = true;
+    uploadResultMessage = error
+  });
+}
+</script>
+
+<style>
+.drop-zone {
+  margin: 50px;
+  background-color: #ffffff;
+  min-width: 200px;
+  height: 200px;
+  padding: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-family: "Quicksand", sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  cursor: pointer;
+  color: #cccccc;
+  border: 4px dashed #7082D0;
+  border-radius: 10px;
+}
+
+.drop-zone--over {
+  border-style: solid;
+}
+
+.drop-zone__input {
+  display: none;
+}
+
+.drop-zone__prompt {
+  color: var(--primary-color);
+}
+
+ul {
+  list-style-type: none;
+}
+
+.upload-button {
+  margin-top: 25px;
+  margin-bottom: 25px;
+}
+</style>
