353fd7442031ee6ff70dcf601e9e71807e30c3cd
Evan Bauer <evanjb@att.net>
2023-05-03 21:37:26 -0500

    assets directory

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <HeroImage text="Karla and Evan" :image="`url('../images/dreams_royal_beach_overhead_view.webp')`" />
+        <HeroImage text="Karla and Evan" :image="`url('../assets/dreams_royal_beach_overhead_view.webp')`" />
     </div>
 </template>
