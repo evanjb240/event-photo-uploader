f5faa87e95af438b0e69c0307449b8e698570b90
Evan Bauer <evanjb@att.net>
2023-05-04 08:29:02 -0500

    Guidelines page

---
+++ src/pages/Guidelines.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div class="column-container">
+            <div class="column">
+                <div class="column-section">
+                    <h3>What to Bring</h3>
+                    <ul>
+                        <li>Light Jacket/Sweatshirt: Evenings can become cool and/or windy</li>
+                        <li>Tip money (USD), plan for roughly: 
+                            <ul>
+                                <li>$5/meal</li>
+                                <li>$1-2 every/every other drink</li>
+                                <li>$2-5 every above and beyond service</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <div class="column-section">
+                    <h3>Be <b>Prepared</b>, Be <b>Cautious</b></h3> 
+                    <ul>
+                        <li>Check in and Lock up: 
+                            <ol>
+                                <li>Put passport and money into safe</li>
+                                <li>Each day, go into safe and grab your tip money you intend to use for the day</li>
+                            </ol>
+                        </li>
+                        <li>There is a lot of local vendors and mini excursions along this beach. Be prepared for some soliciting from bracelets to paragliding.
+                        </li>
+                    </ul>               
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.column-container{
+    display:flex;
+    justify-content: space-between;
+}
+.column{
+    width:100%;
+    display:flex;
+    flex-direction: column;
+}
+.column-section{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+</style>
