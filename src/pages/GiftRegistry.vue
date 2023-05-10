fdda3e3efc51b09362c9754b62063b742e3141ab
Evan Bauer <evanjb@att.net>
2023-05-10 10:40:34 -0500

    Font stuff

--- src/pages/GiftRegistry.vue
+++ src/pages/GiftRegistry.vue
@@ -1,19 +1,53 @@
 <template>
     <div>
-        <h1>
+        <h1 class="centered">
             Gift Registry
         </h1>
-        <h2>
+        <p class="centered">
             Your presence and time with us in Punta Cana is truly enough. However, if you still wish to send a gift, we are registered at Amazon, Target and Crate & Barrel. Please click the store below.  
-        </h2>
-        <div>
-            <a href="https://www.target.com/gift-registry/gift/karla-and-evan"><img src="@/public/images/TargetLogo.jpg" width="75" height="75"></a>
-            <a href="https://www.amazon.com/wedding/share/karla-and-evan"><img src="@/public/images/AmazonLogo.webp" width="150" height="50"></a>
+        </p>
+        <div class="centered">
+            <a class="card" href="https://www.target.com/gift-registry/gift/karla-and-evan"><img src="@/public/images/TargetLogo.jpg" width="75" height="75"></a>
+            <a class="card" href="https://www.amazon.com/wedding/share/karla-and-evan"><img src="@/public/images/AmazonLogo.webp" width="150" height="50"></a>
         </div>
-        <div>
+        <p class="centered">
             If you prefer a gift of cash, we have set up an account with Joy to contribute to our bathroom remodel.
-        </div>
+        </p>
     </div>
 </template>
 <script setup>
 </script>
+<style scoped>
+.centered{
+  display:flex;
+  justify-content: center;
+  text-align: center;
+  gap:20px;
+  align-items: center;
+
+}
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 20%;
+  height: 100px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+h1 {
+	font-family: 'Dancing Script', cursive;
+	font-size: 80px;
+  font-weight: 900;
+	margin-bottom: 30px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+p{
+  font-size: 18px;
+}
+</style>
--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -2,15 +2,15 @@
     <div>
       <HeroImage text="Dreams Royal Beach" :image="`url('/images/dreams_royal_beach_overhead_view.webp')`" />
       <div>
-        <div>
+        <p>
           We have selected Dreams Royal Beach by Hyatt in Punta Cana! Dreams Royal Beach is an all-inclusive resort located on the palm-studded white-sands of Bavaro Beach. It is an intimate, family-friendly resort that includes tons of amenities.
-        </div>
-        <div>
+        </p>
+        <p>
           We would recommend staying from January 5th to January 8th, but feel free to stay longer!
-        </div>
-        <div>
+        </p>
+        <p>
           For those traveling without kids, there is also an option to stay at Secrets Royal Beach. We ____ discount rates. Please use the booking code ___.
-        </div>
+        </p>
         <h3>Booking</h3>
         <ul>
           <li>Resort: <a href="https://www.amrcollection.com/en/resorts-hotels/dreams/dominican-republic/royal-beach-punta-cana/" target="_blank">Dreams Royal Beach, Punta Cana</a></li>
@@ -25,5 +25,10 @@ import HeroImage from '@/components/HeroImage.vue';
 <style scoped>
 .warning{
   color:red
+}
+p{
+  display:flex;
+  justify-content: center;
+  text-align: center;
 }
 </style>
