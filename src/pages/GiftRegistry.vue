7cb944fff15acd8609df689b39c086b703093899
Evan Bauer <evanjb@att.net>
2023-05-10 11:02:14 -0500

    min width for logos

--- src/pages/GiftRegistry.vue
+++ src/pages/GiftRegistry.vue
@@ -7,8 +7,8 @@
             Your presence and time with us in Punta Cana is truly enough. However, if you still wish to send a gift, we are registered at Amazon, Target and Crate & Barrel. Please click the store below.  
         </p>
         <div class="centered">
-            <a class="card" href="https://www.target.com/gift-registry/gift/karla-and-evan"><img src="@/public/images/TargetLogo.jpg" width="75" height="75"></a>
-            <a class="card" href="https://www.amazon.com/wedding/share/karla-and-evan"><img src="@/public/images/AmazonLogo.webp" width="150" height="50"></a>
+            <a class="card" style="min-width:100px" href="https://www.target.com/gift-registry/gift/karla-and-evan"><img src="@/public/images/TargetLogo.jpg" width="75" height="75"></a>
+            <a class="card" style="min-width:175px" href="https://www.amazon.com/wedding/share/karla-and-evan"><img src="@/public/images/AmazonLogo.webp" width="150" height="50"></a>
         </div>
         <p class="centered">
             If you prefer a gift of cash, we have set up an account with Joy to contribute to our bathroom remodel.
