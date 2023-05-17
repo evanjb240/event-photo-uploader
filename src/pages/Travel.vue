bdd58d1acf708f1e72931367a780f0290a6233fe
Evan Bauer <evanjb@att.net>
2023-05-16 19:30:59 -0500

    travel page

--- src/pages/Travel.vue
+++ src/pages/Travel.vue
@@ -1,7 +1,31 @@
 <template>
-    <div>
-        Travel
+    <div class="travel-container">
+        <h1>
+            Travel
+        </h1>
+        <hr/>
+        <div>
+            <h3>Wedding</h3>
+            <p>Our wedding will be on Sunday, January 7, 2024. However, everyone is invited to the rehearsal dinner on Saturday, January 6, 2024.  We recommend flying in on January 5th, but no later than January 6th so you do not miss out on any of the festivities!</p> 
+            <p>We hope you will treat this as a vacation and spend some time with us at the resort. We recommend staying January 5, 2024 to January 8, 2024.</p>
+        </div>
+        <div>
+            <h3>Airport</h3>
+            <p>You will need to fly into Punta Cana International Airport (PUJ).  The airport is about 20-25 minutes away from the resort.</p>
+        </div>
+        <div>
+            <h3>Venue</h3>
+            <p>Our venue is Dreams Royal Beach. It is an all inclusive resort in the Hyatt family. Visit our <RouterLink to="/Venue">Venue Page</RouterLink> to find out more and book your stay!</p>
+        </div>
+        <div>
+            <h3>Transportation</h3>
+            <p>You will need to book transportation from the Airport to the resort. We recommend ____. The cost is ___. There is no need to get a rental car.</p>
+        </div>  
     </div>
 </template>
-<script setup>
-</script>
+<style scoped>
+.travel-container{
+    display:flex;
+    flex-direction: column;
+}
+</style>
