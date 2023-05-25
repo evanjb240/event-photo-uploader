cf2d037629216725e5762ce938e1956ff1ca3340
Evan Bauer <evanjb@att.net>
2023-05-24 22:53:18 -0500

    added info

--- src/pages/Guidelines.vue
+++ src/pages/Guidelines.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <div class="column-container">
+        <h1 class="centered">
+            FAQ's
+        </h1>
+        <!--<div class="column-container">
             <div class="column">
                 <div class="column-section">
                     <h3>What to Bring</h3>
@@ -31,7 +34,46 @@
                     </ul>               
                 </div>
             </div>
+        </div>-->
+        <h2>We will continue to update this page as more questions come in!</h2>
+ 
+        <div class="question">When should I book my hotel/resort stay?</div>
+        <div class="answer">We recommend booking your room as soon as possible. The resort will fill up quickly and there is nothing we can do if the resort sells out.</div>
+        <div class="question">How long can I stay at the resort?</div>
+        <div class="answer">You can stay at the resort as long as you want! We recommend from at least January 5th to January 8th.</div>
+        <div class="question">Is the resort all-inclusive?</div>
+        <div class="answer">The resort is all-inclusive. Once at the resort you only need money for tips and shopping.</div>
+        <div class="question">Do I need a Passport?</div>
+        <div class="answer">Yes. Passports are taking an average of 8 weeks. If you need to renew or apply for a passport, we recommend doing it as soon as possible. Find more information here: https://travel.state.gov/content/travel/en/passports/how-apply.html</div>
+        <div class="question">Do I need immigration forms to enter Punta Cana?</div>
+        <div class="answer">Yes. Punta Cana has a digital process for filling out its immigration forms. Fill out your entry forms here: https://eticket.migracion.gob.do Once you complete the forms, you will receive a QR code. Keep these QR codes (also called e-tickets) to show at immigration after your deplane.</div>
+        <div class="question">Does the resort have a shuttle from the airport?</div>
+        <div class="answer">No. You will need to book your transportation from the airport to the resort. We recommend using AmstarDMC.  You can book here: www.AmstarDMC.com.</div>
+        <div class="answer">It will take about an hour to go through immigration and customs, so you should book your shuttle for an hour after your flight arrival time.</div>
+        <div class="question">Do I have to go through customs?</div>
+        <div class="answer">Yes. You will go through immigration and customs when you enter Punta Cana and when you re-enter the US.</div>
+        <div class="question">What are “must items” that should I pack?</div>
+        <div class="answer">
+            <ul>
+                <li>Your party vibes!</li>
+                <li>Passport</li>
+                <li>Cash for tips and shopping (USD is fine)</li>
+                <li>Sunscreen! (and aloe if you burn)</li>
+                <li>Sunglasses</li>
+                <li>Swimsuits (we recommend multiple)</li>
+                <li>Light jacket (weather will be highs of 75-80 degrees, but lows of around 65 degrees)</li>
+                <li>Wedding attire</li>
+                <li>Dress clothes for the seafood restaurant (men need long pants)</li>
+            </ul>
+            <div>Optional:</div>
+            <ul>
+                <li>Floaties for the pool</li>
+                <li>Towel (we had no issue getting these at the resort)</li>
+                <li>Tennis shoes if you are going to exercise or plan to walk a lot</li>
+            </ul>
         </div>
+        <div class="question">What is the primary language in Punta Cana?</div>
+        <div class="answer">The primary language is spanish. However, almost everyone at the resort speaks english. </div>
         <WeddingLogo/>
     </div>
 </template>
@@ -49,5 +91,14 @@
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+}
+
+.question{
+    padding: 5px;
+    font-weight: 900;
+    font-size: 18px;
+}
+.answer{
+    padding: 5px;
 }
 </style>
--- src/pages/Travel.vue
+++ src/pages/Travel.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="travel-container">
-        <h1>
+        <h1 class="centered">
             Travel
         </h1>
         <WeddingLogo :width="150" :height="150"/>
@@ -11,12 +11,23 @@
             <p>We hope you will treat this as a vacation and spend some time with us at the resort. We recommend staying January 5, 2024 to January 8, 2024.</p>
         </div>
         <div>
-            <h3>Airport</h3>
-            <p>You will need to fly into Punta Cana International Airport (PUJ).  The airport is about 20-25 minutes away from the resort.</p>
+            <h3>RSVP Deadline</h3>
+            <p>Please RSVP no later than October 1, 2023. Your RSVP is not considered complete until you have a room booked at the resort.</p>
+            <p>Please click <NuxtLink to="/RSVP">HERE</NuxtLink> to RSVP.</p>
+            <p>Both the rehearsal dinner and wedding dinner will be buffet style. Please note any dietary restrictions when you RSVP.</p>
         </div>
         <div>
             <h3>Venue</h3>
             <p>Our venue is Dreams Royal Beach. It is an all inclusive resort in the Hyatt family. Visit our <RouterLink to="/Venue">Venue Page</RouterLink> to find out more and book your stay!</p>
+        </div>
+        <div>
+            <h3>Passport, Immigration & Customs</h3>
+            <p>A passport is required to travel to and from Punta Cana from the U.S. Passports are taking an average of 8 weeks to be completed. We recommend applying for your passport as soon as possible.</p>
+            <p>You will need to fill out immigration and custom forms to enter Punta Cana. See our <RouterLink to="/Guidelines">Tips and Tricks and FAQ page</RouterLink> for more details!</p>
+        </div>
+        <div>
+            <h3>Airport</h3>
+            <p>You will need to fly into Punta Cana International Airport (PUJ).  The airport is about 20-25 minutes away from the resort.</p>
         </div>
         <div>
             <h3>Transportation</h3>
