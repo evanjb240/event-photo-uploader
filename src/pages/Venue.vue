195fc8ed90da5ef83a19e94d87e1568416646f6b
Evan Bauer <evanjb@att.net>
2023-06-28 17:09:28 -0500

    venue changes

--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -2,28 +2,17 @@
     <div>
       <HeroImage text="Dreams Royal Beach" :image="`url('/images/dreams_royal_beach_overhead_view.webp')`" />
       <div>
-        <p class="centered">
-          We have selected Dreams Royal Beach by Hyatt in Punta Cana! Dreams Royal Beach is an all-inclusive resort located on the palm-studded white-sands of Bavaro Beach. It is an intimate, family-friendly resort that includes tons of amenities.
-        </p>
-        <p class="centered">
-          Check out the room types and amenities here
-        </p>
+        <p class="centered">We have selected Dreams Royal Beach by Hyatt in Punta Cana! Dreams Royal Beach is an all-inclusive resort located on the palm-studded white-sands of Bavaro Beach. It is an intimate, family-friendly resort that includes tons of amenities.</p>
+        <h3>Booking</h3>
+        <p>We have secured a group booking code that provides a discounted room rate. In order to secure your room, you must book using this code and pay a 1-night deposit. The remainder is due 14 days prior to your stay (charged to same card you booked with).  Cancellation and change fees apply.</p>
+        <p>We would recommend staying at least from January 5th to January 8th, but feel free to stay longer! We have a discount code available for 1/3-1/9. If you want to stay outside of those dates, you will be best off calling the resort directly to book your stay.</p>
         <div class="primary-text centered">
             <a class="primary-button" href="https://www.amrcollection.com/en/resorts-hotels/dreams/dominican-republic/royal-beach-punta-cana/" target="_blank">Dreams Royal Beach</a>
         </div>
-        <p>There is an option to stay club level or preferred. This option gives you exclusive access to certain features of the resort and a personal butler. We enjoyed having a personal butler, but it is not necessary.</p>
-        <p>
-          For those traveling without kids, there is also an option to stay at Secrets Royal Beach.
-        </p>
-        <h3>Booking</h3>
-        <p>We have secured a group booking code that provides a discounted room rate. In order to secure your room, you must book using this code and pay a 1-night deposit. The remainder is due 14 days prior to your stay (charged to same card you booked with).  There are cancellation fees depending on when you cancel.</p>
-        <p>
-          We would recommend staying at least from January 5th to January 8th, but feel free to stay longer! We have a discount code available for 1/3-1/9. If you want to stay outside of those dates, you will be best off calling the resort directly to book your stay.
-        </p>
         <p>Booking Code: NBW0124</p>
         <p>Please read through the notes below before jumping to resort website. Step by step instructions are also below.</p>
         <p><u>Booking Notes:</u></p>
-        <p>We have secured a group booking code with the resort. The code provides a good discount on select rooms, but does require a deposit and has change and cancellation fees.  As an example, using the code (subject to availability) you can book a 5-night stay for 2 adults for $1,890 total ($945 per person).</p>
+        <p>We recommend at least staying Jan 5th - 8th, but feel free to stay longer! We have secured a group booking code with the resort. The code applies to Jan 3 - Jan 9 with check out on the 10th. You can book outside of these dates but the code only applies to these nights. The code provides a decent discount on select rooms, but does require a deposit and has change and cancellation fees.  As an example, using the code (subject to availability) you can book a 5-night stay for 2 adults for $1,890 total ($945 per person).</p>
         <p>The code applies to the following 5 room types:</p>
         <ul>
           <li>Deluxe Family Room</li>
@@ -45,18 +34,23 @@
           <li>You can book at the adults-only resort (Secrets) next door. Secrets’ guests have full access to Dreams. However, we do not have a discount code for Secrets at this time.</li>
         </ul>
 
-        <p><u>Step by Step Booking Instructiions</u></p>
-        <ol>
-          <li>Go to the resort website Accomodations->Rooms & Suites page and look at the various room types for regular and club level.</li>
-          <li>After you decide on your room type, go to the check availability bar. Enter your travel dates, number of rooms, number of guests, and select "Resort Only". DO NOT book your flight through the resort (it's a messy process). Click the “Check Availability” button.</li>
-          <li>On the next screen, under “Trip summary” click the “Enter discount code” button.</li>
-          <li>A white box should pop up on your screen. Enter the discount code in the open field and click “Save”. Look at that discount!</li>
-          <li>You'll be able to select the room by clicking on the price, which will expand a section to confirm your selection. You'll notice that roughly 20% is needed to book. Click "Select" to continue</li>
-          <li>The next screen allows you to add some services, especially useful for airport transportation. If you have not bought your flight tickets yet, it'll likely be easier to contact the resort once you do, to schedule an airport transfer. There are other services below though so check those out too!</li>
-          <li>For those who have bought flight tickets, have the flight number ready. You'll want to enter the flight info into the vehicle size of your choice and click the '+' button for each person riding with you to add to cart.</li>
-          <li>Once you have added any applicable services, click Continue.</li>
-          <li>Fill out the remaining form portions, click 'Reserve' and we'll see you down there!</li>
-        </ol>
+        <p><u>How to book</u></p>
+        <ul>
+          <li><u>Option 1:</u> You can use our Call Center by dialing 1-866-237-3267, make sure you inform the agent of the group code or you will be charged the regular rate, only by providing the group code will the agent be able to confirm your reservation with the discounted rates.</li>
+          <li><u>Option 2:</u> You can book at the website (if you are staying within the discount date range):
+            <div class="primary-text centered" style="margin-bottom:20px;margin-top:20px;">
+              <a class="primary-button" href="https://www.amrcollection.com/en/resorts-hotels/dreams/dominican-republic/royal-beach-punta-cana/" target="_blank">Dreams Royal Beach</a>
+            </div>
+            <ol>
+              <li>Select the amount of guests who will occupy the room and select your travel dates between January 3 & 9, 2024</li>
+              <li>Select "Resort Only". DO NOT book your flight through the resort (it's a messy process). Click the “Check Availability” button.</li>
+              <li>You will be redirected to the reservation site, click on “Enter Discount Code” and type the group code all caps-no spaces as the website is caption sensitive</li>
+              <li>Click on “Save” to see the rooms with the discounted rates</li>
+              <li>Here you just need to select the room type you want to reserve by clicking on it and follow the reservation steps</li>
+              <li>Once your 1 night deposit is charged you will receive an email with your room confirmation as well as a link to modify your reservation if needed.</li>
+            </ol>
+          </li>
+        </ul>
       </div>
       <WeddingLogo/>
     </div>
