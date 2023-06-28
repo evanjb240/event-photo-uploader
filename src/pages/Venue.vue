ecef523b9f07cc8891b0b19b1323d7d85c3e244c
Evan Bauer <evanjb@att.net>
2023-06-28 09:12:18 -0500

    Booking Instructions

--- src/pages/Venue.vue
+++ src/pages/Venue.vue
@@ -18,16 +18,45 @@
         <h3>Booking</h3>
         <p>We have secured a group booking code that provides a discounted room rate. In order to secure your room, you must book using this code and pay a 1-night deposit. The remainder is due 14 days prior to your stay (charged to same card you booked with).  There are cancellation fees depending on when you cancel.</p>
         <p>
-          We would recommend staying from January 5th to January 8th, but feel free to stay longer!
+          We would recommend staying at least from January 5th to January 8th, but feel free to stay longer! We have a discount code available for 1/3-1/9. If you want to stay outside of those dates, you will be best off calling the resort directly to book your stay.
         </p>
-        <p>Booking Code:</p>
-        <p>Booking Notes</p>
+        <p>Booking Code: NBW0124</p>
+        <p>Please read through the notes below before jumping to resort website. Step by step instructions are also below.</p>
+        <p><u>Booking Notes:</u></p>
+        <p>We have secured a group booking code with the resort. The code provides a good discount on select rooms, but does require a deposit and has change and cancellation fees.  As an example, using the code (subject to availability) you can book a 5-night stay for 2 adults for $1,890 total ($945 per person).</p>
+        <p>The code applies to the following 5 room types:</p>
         <ul>
-          <li>Cancellation fees do apply. Before canceling contact us as we may have a guest who has not booked and could use your room.</li>
-          <li>If you do not book through the resort website, you are not considered part of the group.</li>
-          <li>Three adults maximum in a room.</li>
-          <li>Children are ages 12 and under. Children over 12 are considered adults but do not count towards the three adults maximum per room.</li>
+          <li>Deluxe Family Room</li>
+          <li>Deluxe Pool View (mix of kings and doubles)</li>
+          <li>Deluxe Pool Terrace (mix of kings and doubles)</li>
+          <li>Preferred Deluxe Swim up</li>
+          <li>Preferred Deluxe Ocean view</li>
         </ul>
+        <p>We recommend booking with the code as these are the cheapest rates we have found. However, the code does not guarantee availability, so we recommend booking your room within the next 3 months because the resort will begin to fill-up quickly.  Unfortunately, there is nothing we can do if the resort sells out. </p>
+        <p>There is an option to stay club level or preferred. This option gives you exclusive access to certain features of the resort and a personal butler. We enjoyed having a personal butler, but it is not necessary.</p>
+        <p><u>Booking notices:</u></p>
+        <ul>
+          <li>Cancellation and change fees apply if you use the booking code</li>
+          <li>Do not book your flight through the resort. For more information on flights go to our Tips and Tricks page.</li>
+          <li>If you do not book through the resort website, the information above may not apply. You also will not be listed as part of the “group” and we will have limited ability to help answer questions or deal with reservation issues.</li>
+          <li>Three adults maximum in most rooms.</li>
+          <li>Children are ages 12 and under. Children 2 and under are free.</li>
+          <li>Children over 12 are considered adults for booking purposes. If you have children over 12, you’ll be able to book certain rooms that do not have the 3 adult maximum.</li>
+          <li>You can book at the adults-only resort (Secrets) next door. Secrets’ guests have full access to Dreams. However, we do not have a discount code for Secrets at this time.</li>
+        </ul>
+
+        <p><u>Step by Step Booking Instructiions</u></p>
+        <ol>
+          <li>Go to the resort website Accomodations->Rooms & Suites page and look at the various room types for regular and club level.</li>
+          <li>After you decide on your room type, go to the check availability bar. Enter your travel dates, number of rooms, number of guests, and select "Resort Only". DO NOT book your flight through the resort (it's a messy process). Click the “Check Availability” button.</li>
+          <li>On the next screen, under “Trip summary” click the “Enter discount code” button.</li>
+          <li>A white box should pop up on your screen. Enter the discount code in the open field and click “Save”. Look at that discount!</li>
+          <li>You'll be able to select the room by clicking on the price, which will expand a section to confirm your selection. You'll notice that roughly 20% is needed to book. Click "Select" to continue</li>
+          <li>The next screen allows you to add some services, especially useful for airport transportation. If you have not bought your flight tickets yet, it'll likely be easier to contact the resort once you do, to schedule an airport transfer. There are other services below though so check those out too!</li>
+          <li>For those who have bought flight tickets, have the flight number ready. You'll want to enter the flight info into the vehicle size of your choice and click the '+' button for each person riding with you to add to cart.</li>
+          <li>Once you have added any applicable services, click Continue.</li>
+          <li>Fill out the remaining form portions, click 'Reserve' and we'll see you down there!</li>
+        </ol>
       </div>
       <WeddingLogo/>
     </div>
@@ -39,9 +68,4 @@ import HeroImage from '@/components/HeroImage.vue';
 .warning{
   color:red
 }
-/*p{
-  display:flex;
-  justify-content: center;
-  text-align: center;
-}*/
 </style>
